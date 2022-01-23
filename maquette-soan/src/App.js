533bfbcf04e3eb5e3f57b9b0a6dc8ea081dda22d
Paulo <90481933+Paulo54360@users.noreply.github.com>
2022-01-23 19:37:54 +0100

    iniate programme

---
+++ maquette-soan/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PayForm from "./component/PayForm";
+function App() {
+  return (
+    <div className="App">
+      <PayForm/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ maquette-soan/src/component/PayForm.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+class payForm extends React.Component {
+render (){
+    return (
+        <div className="FormModal">
+                <h1>Paiement sécurisé par prélèvement bancaire</h1>
+        </div>
+
+    )
+}
+
+}
